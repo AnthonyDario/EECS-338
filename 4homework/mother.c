65e5b8e38d7bc77c8c46212cbc32d24650cc73d1
Anthony Dario <anthonyrdario@gmail.com>
2015-11-20 15:05:05 -0500

    Counting cookies
    I'll take vegas by storm

--- 4homework/mother.c
+++ 4homework/mother.c
@@ -2,6 +2,7 @@
 #include "cookie.h"
 
 int cookies = 20;
+int tinaCount = 0;
 
 // gives a cookie to Tina or Judy
 int get_me_my_cookie(int a, int b) {
@@ -15,6 +16,8 @@ int get_me_my_cookie(int a, int b) {
     // if this is Tina then give her a cookie
     if (b == 1) {
         printf("You can have a cookie Tina\n");
+        tinaCount++;
+        cookies--;
         return 1;
     }
 
@@ -22,9 +25,12 @@ int get_me_my_cookie(int a, int b) {
     if (b == 0) {
         if (tinaCount < 2) {
             printf("You may have a cookie Judy\n");
+            cookies--;
+            tinaCount = 0;
             return -1;
         } else {
-            printf("You have to wait for Tina before getting a cookie Judy\n");
+            printf("You have to wait for Tina to get %d more ", 2 - tinaCount)
+            printf("cookies before getting a cookie Judy\n");
             return 1;
         }
     }
